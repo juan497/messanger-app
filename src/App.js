c92364ceed891d09c17570832baf9b37d80a0f31
Juan Hernandez <juanhernandez9731@gmail.com>
2025-03-22 21:41:30 -0700

    made a message react component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from '@mui/material';
 import { useState } from 'react';
+import Message from './components/Message';
 
 
 function App() {
@@ -31,7 +32,7 @@ function App() {
 
       {
         messages.map(message =>(
-          <p>{message}</p>
+          <Message text={message}/>
         ))
       }
     </div>
---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+
+//with the es7 extension type rfce to make boiler plate component.
+import React from 'react'
+
+function Message(props) {
+  return (
+    <div>
+        <h2>{props.text}</h2>
+    </div>
+  )
+}
+
+export default Message
